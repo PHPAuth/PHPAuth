c420b6483a4a3d2be602f45e11a2988a964138d6
Karel Wintersky <karel.wintersky@gmail.com>
2015-09-08 05:18:36 +0300

    Update config.class.php
    It is a little trick: 
    We can call `$config = new Config($dbh, "config_table");` **always** 
    OR 
    we can *patch* `private $phpauth_config_table` manually and calls `$config = new Config($dbh);` without any additional values (special for lazy coders like me ;-) )

--- config.class.php
+++ config.class.php
@@ -1,14 +1,29 @@
 <?php
 
+/**
+ * PHPAuth config class
+ */
 class Config
 {
     private $dbh;
     private $config;
     private $phpauth_config_table = 'config';
 
-    public function __construct(\PDO $dbh)
+    /**
+     * Construct Config class, that contains all configuration values.
+     * 
+     * It is a little trick: we can call '$config = new Config($dbh, "config_table");' always or
+     * we can patch private $phpauth_config_table manually and calls '$config = new Config($dbh);'
+     * without any additional values!
+     * @param \PDO $dbh             -- PDO database connect handler
+     * @param string $config_table  -- config table name
+     */
+    public function __construct(\PDO $dbh, $config_table = 'config')
     {
         $this->dbh = $dbh;
+        
+        if (isset($config_table))
+            $this->phpauth_config_table = $config_table;
 
         $this->config = array();
 
@@ -20,11 +35,22 @@ class Config
         }
     }
 
+    /**
+     * Return config value
+     * @param $setting  -- key
+     * @return mixed    -- config value
+     */
     public function __get($setting)
     {
         return $this->config[$setting];
     }
 
+    /**
+     * Set config value
+     * @param $setting  -- key
+     * @param $value    -- config value
+     * @return bool     -- true if no any errors occured
+     */
     public function __set($setting, $value)
     {
         $query = $this->dbh->prepare("UPDATE {$this->phpauth_config_table} SET value = ? WHERE setting = ?");
