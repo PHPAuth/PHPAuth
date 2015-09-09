d030a241c1d7593b782ecf715ba9f4bfb30cdb6e
karel.wintersky <karel.wintersky@gmail.com>
2015-09-09 17:08:49 +0300

    Namespace added
    + Namespace added for auth.class.php and config.class.php
    + fixed README.md
    + fixed PHPDocs

--- config.class.php
+++ config.class.php
@@ -1,34 +1,41 @@
 <?php
+namespace PHPAuth;
+
+/**
+ *
+ */
 class Config
 {
     private $dbh;
     private $config;
-    private $config_table;
+    private $config_table = 'config';
 
     /**
      * Config::__construct()
-     * 
-     * @param mixed $dbh
+     *
+     * @param \PDO $dbh
      * @param string $config_table
-     * @return
      */
     public function __construct(\PDO $dbh, $config_table = 'config')
     {
         $this->dbh = $dbh;
-        $this->phpauth_config_table = $config_table;
-        
+
+        if (func_num_args() > 1)
+            $this->phpauth_config_table = $config_table;
+
         $this->config = array();
 
         $query = $this->dbh->query("SELECT * FROM {$this->phpauth_config_table}");
 
-        while($row = $query->fetch()) {
+        while ($row = $query->fetch()) {
             $this->config[$row['setting']] = $row['value'];
         }
     }
+ 
 
     /**
      * Config::__get()
-     * 
+     *
      * @param mixed $setting
      * @return
      */
@@ -39,19 +46,19 @@ class Config
 
     /**
      * Config::__set()
-     * 
+     *
      * @param mixed $setting
      * @param mixed $value
-     * @return
+     * @return bool
      */
     public function __set($setting, $value)
     {
         $query = $this->dbh->prepare("UPDATE {$this->phpauth_config_table} SET value = ? WHERE setting = ?");
 
-        if($query->execute(array($value, $setting))) {
+        if ($query->execute(array($value, $setting))) {
             $this->config[$setting] = $value;
             return true;
-        } 
+        }
         return false;
     }
 }
