e41ee92ea0cd1eafd73b44512f2cf5c71d3173b0
Philip Lundqvist <philiplundqvist@gmail.com>
2017-12-27 16:11:25 +0100

    Scan directory for language files, read and insert in sql.

--- languages/converter.php
+++ languages/converter.php
@@ -1,29 +1,48 @@
 <?php
 
-$lang = array();
-require "en_GB.php";                // PHPAuth-compatible language-file to convert
-
 $config = array();
 $config['name'] = "test";           // Database name
 $config['host'] = "localhost";      // Database host
 $config['user'] = "test";           // Database user
 $config['pass'] = "123456789";      // Database user password
 
-/* ---------------------------------
-    Do not alter below this line
-----------------------------------*/
-
 $dbh = new PDO('mysql:dbname=' . $config['name'] . ';host=' . $config['host'] . ';charset=utf8', $config['user'], $config['pass']);
 
-$langid = 1;
+$files = scandir(__DIR__);
 
-foreach( $lang as $key => $text ){
-
-    $query = $dbh->prepare("INSERT INTO translations (`lang`, `key`, `text`) VALUES (?, ?, ?)");
-    if(!$query->execute(array($langid, $key, $text))){
-        echo "FAIL: " . $key . "\n\n";
+foreach($files as $file)
+{
+    if(is_file($file) && $file != "converter.php")
+    {
+        $lang = array();
+        require $file;
+        
+        $code = explode("_", $file);
+        $code = $code[0];
+        
+        $query = $dbh->prepare("INSERT INTO languages (`lang`) VALUES (?)");
+        
+        if($query->execute(array($code)))
+        {
+            echo "\n" . "SUCCESS: " . $code . "\n";
+            
+            $langid = $dbh->lastInsertId();
+            
+            foreach($lang as $key => $text)
+            {
+                $query = $dbh->prepare("INSERT INTO translations (`lang`, `key`, `text`) VALUES (?, ?, ?)");
+                
+                if($query->execute(array($langid, $key, $text)))
+                {
+                    echo "SUCCESS: " . $key . "\n";
+                } else {
+                    echo "FAIL: " . $key . "\n";
+                }
+            }
+        } else {
+            echo "\n" . "FAIL: " . $code . "\n";
+        }
     }
-
 }
 
 ?>
