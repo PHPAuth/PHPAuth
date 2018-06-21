abb9fdb76ba15345b86ef455e266177a1c774303
Kruno Knego <knego.kruno@gmail.com>
2018-06-21 16:21:58 +0200

    Updated config with the correct translation table name

--- database_defs/database_pgsql.sql
+++ database_defs/database_pgsql.sql
@@ -45,7 +45,7 @@ INSERT INTO phpauth_config (setting, value) VALUES
 ('table_sessions',  'phpauth_sessions'),
 ('table_users', 'phpauth_users'),
 ('table_emails_banned', 'phpauth_emails_banned'),
-('table_translations', 'phpauth_translations'),
+('table_translations', 'phpauth_translation_dictionary'),
 ('verify_email_max_length', '100'),
 ('verify_email_min_length', '5'),
 ('verify_email_use_banlist',  '1'),
