3e96ded9b5d8062120a01294ddfaa74c700e0877
zhangchenglin <469946668@qq.com>
2021-02-06 06:35:51 +0800

    edit text

--- languages/en_GB.php
+++ languages/en_GB.php
@@ -71,6 +71,6 @@ $lang['account_deleted'] = "Account deleted successfully.";
 $lang['function_disabled'] = "This function has been disabled.";
 $lang['account_not_found'] = "No account found with that email address";
 
-$lang['php_version_required'] = "PHP %s+ required for PHPAuth engine!";
+$lang['php_version_required'] = "PHPAuth engine requires PHP version %s+!";
 
 return $lang;
